62e04cfb906cfc1f2c8f1213da8be56ccaf07cb7
who i am <mrtevos17@gmail.com>
2022-01-28 10:19:24 +0700

    update

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -63,14 +63,14 @@
                 <li class="nav-item dropdown pe-3">
 
                     <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                        <img src="{{ asset('assets/img/profile-img.jpg') }}" alt="Profile" class="rounded-circle">
-                        <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+                        <img src="{{ asset('https://ui-avatars.com/api/?name=Achmad-Andybackground=0D8ABC&color=fff') }}" alt="Profile" class="rounded-circle">
+                        <span class="d-none d-md-block dropdown-toggle ps-2">A.A. Dekanovy</span>
                     </a><!-- End Profile Iamge Icon -->
 
                     <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                         <li class="dropdown-header">
-                            <h6>Kevin Anderson</h6>
-                            <span>Web Designer</span>
+                            <h6>Achmad Andy Dekanovy</h6>
+                            <span>Karyawan</span>
                         </li>
                         <li>
                             <hr class="dropdown-divider">
