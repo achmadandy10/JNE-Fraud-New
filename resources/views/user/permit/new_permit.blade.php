00bc167b197ef134be1916d80799fecb86e04d48
fresaletlora <mikhaelfresa@gmail.com>
2022-01-28 14:25:53 +0700

    update all tampilan

--- resources/views/user/permit/new_permit.blade.php
+++ resources/views/user/permit/new_permit.blade.php
@@ -16,93 +16,63 @@
                 <!-- Horizontal Form -->
                 <form>
                     <div class="row mb-3">
-                        <div class="col-md-6">
+                        <div class="col-md-12">
                             <div class="row mb-3">
-                                <label for="submission_number" class="col-sm-6 col-form-label">Nomor Kasus</label>
-                                <div class="col-sm-6">
-                                    <input type="text" name="submission_number" class="form-control"
-                                        id="submission_number">
+                                <label for="submission_number" class="col-sm-4 col-form-label">Tipe Perizinan</label>
+                                <div class="col-sm-8">
+                                    <select name="" id="" class="form-control">
+                                        <option value="#">--Pilih--</option>
+                                        <option value="">Perizinan Reklame</option>
+                                        <option value="">Perizinan IMB</option>
+                                        <option value="">Perizinan SLF</option>
+                                        <option value="">Perizinan TDG</option>
+                                        <option value="">OSS</option>
+                                    </select>
                                 </div>
                             </div>
                             <div class="row mb-3">
-                                <label for="first_party" class="col-sm-6 col-form-label">Tanggal</label>
-                                <div class="col-sm-6">
+                                <label for="first_party" class="col-sm-4 col-form-label">Lokasi</label>
+                                <div class="col-sm-8">
                                     <input type="text" name="first_party" class="form-control" id="first_party">
                                 </div>
                             </div>
                             <div class="row mb-3">
-                                <label for="second_party" class="col-sm-6 col-form-label">Nama Pihak</label>
-                                <div class="col-sm-6">
+                                <label for="second_party" class="col-sm-4 col-form-label">Spesifikasi</label>
+                                <div class="col-sm-8">
                                     <input type="text" class="form-control">
                                 </div>
                             </div>
                             <div class="row mb-3">
-                                <label for="third_party" class="col-sm-6 col-form-label">Unit/Departemen/Divisi</label>
-                                <div class="col-sm-6">
-                                    <input type="text" class="form-control">
+                                <label for="third_party" class="col-sm-4 col-form-label">Alasan Permohonan</label>
+                                <div class="col-sm-8">
+                                    <textarea name="other_point" class="form-control" id="other_point" cols="10" rows="5"></textarea>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <div class="row mb-3">
-                                <label for="start_date" class="col-sm-6 col-form-label">Nomor Dokumen</label>
-                                <div class="col-sm-6">
-                                    <input type="text" class="form-control">
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <label for="end_date" class="col-sm-6 col-form-label">Total Kerugian/Perkara</label>
-                                <div class="col-sm-6">
-                                    <div class="input-group">
-                                        <span class="input-group-text">Rp</span>
-                                        <input type="number" class="form-control" placeholder="">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-12 mb-3">
-                        <label for="other_point" class="form-label">Kronologis singkat Kejadian :</label>
-                        <textarea name="other_point" class="form-control" id="other_point" cols="30" rows="10"></textarea>
                     </div>
 
                     <div class="row mb-3">
-                        <div class="col-md-2">Bukti :</div>
-                        <div class="col-md-10">
-                            <div class="row mb-3">
-                                <label for="inputNumber" class="col-sm-6 col-form-label">Dokumen</label>
-                                <div class="col-sm-6">
-                                    <input class="form-control" type="file" id="formFile">
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <label for="inputNumber" class="col-sm-6 col-form-label">Bukti 1</label>
-                                <div class="col-sm-6">
-                                    <input class="form-control" type="file" id="formFile">
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <label for="inputNumber" class="col-sm-6 col-form-label">Bukti 2</label>
-                                <div class="col-sm-6">
-                                    <input class="form-control" type="file" id="formFile">
-                                </div>
-                            </div>
-                            <div class="row mb-3">
-                                <label for="inputNumber" class="col-sm-6 col-form-label">Bukti 3</label>
-                                <div class="col-sm-6">
-                                    <input class="form-control" type="file" id="formFile">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row mb-3">
+                        <div class="col-md-3">Dokumen Pendukung</div>
+                        <div class="row mb-3 mt-4">
                             <div class="col-md-4">Disposisi</div>
                             <div class="col-md-5">
                                 <input type="file" class="form-control">
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <div class="col-md-4">Dokumen Lainnya</div>
+                            <div class="col-md-4">Dokumen 1</div>
+                            <div class="col-md-5">
+                                <input type="file" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-md-4">Dokumen 2</div>
+                            <div class="col-md-5">
+                                <input type="file" class="form-control">
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-md-4">Dokumen 3</div>
                             <div class="col-md-5">
                                 <input type="file" class="form-control">
                             </div>
